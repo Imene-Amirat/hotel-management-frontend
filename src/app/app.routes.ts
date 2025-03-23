36a7f6a206a6d28102acfd136ed552b804ef4a98
Imene-Amirat <imene2004.amirat@gmail.com>
2025-03-23 23:02:37 +0100

    Moving auth routes to auth.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { authRoutes } from './features/auth/auth.routes';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent},
+    ...authRoutes,   //take everything inside authRoutes array and insert it here
 ];
