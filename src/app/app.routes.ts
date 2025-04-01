ad39b5f4295f188d13e5245958fc2960662af0e2
Imene-Amirat <imene2004.amirat@gmail.com>
2025-04-02 00:02:23 +0200

    Add route to HomeComponent in app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
 import { authRoutes } from './features/auth/auth.routes';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
     ...authRoutes,   //take everything inside authRoutes array and insert it here
+    {path: '', component: HomeComponent}, //default route
 ];
