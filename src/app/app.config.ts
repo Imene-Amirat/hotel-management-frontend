6a65e640a391d52ed861997461ab7ba956f8ccb1
Imene-Amirat <imene2004.amirat@gmail.com>
2025-04-24 22:54:55 +0100

    Add comments to appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,11 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
+//appConfig is just a custom configuration object you define to group your providers (like provideHttpClient, provideRouter, etc.)
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(),  //tells Angular to register HttpClient as a global provider so your AuthService can use it
     provideAnimationsAsync()
   ]
 };
