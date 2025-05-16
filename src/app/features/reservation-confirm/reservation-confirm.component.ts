ee064e74609d1d3f1051099bd899b28f5b5a11c1
Imene-Amirat <imene2004.amirat@gmail.com>
2025-05-16 11:32:23 +0100

    Add reservation confirmation page

---
+++ src/app/features/reservation-confirm/reservation-confirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './reservation-confirm.component.html',
+  styleUrl: './reservation-confirm.component.scss'
+})
+export class ReservationConfirmComponent {
+
+}
