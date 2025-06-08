f91a9537c105c86b6e161c54ea1eb3d193d9449e
Imene-Amirat <imene2004.amirat@gmail.com>
2025-06-08 11:03:33 +0100

    Implement dynamic feature details display with API integration

--- src/app/features/feature-details/feature-details.component.ts
+++ src/app/features/feature-details/feature-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HotelFeaturesService } from '../../core/services/hotel-features/hotel-features.service';
+import { Feature } from '../../shared/models/feature';
 
 @Component({
   selector: 'app-feature-details',
@@ -8,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrl: './feature-details.component.scss'
 })
 export class FeatureDetailsComponent {
+  apiUrl = 'http://localhost:8080/';
+  feature: Feature | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private featuresService: HotelFeaturesService
+  ) { }
+
+  ngOnInit() {
+    const featureId = this.route.snapshot.paramMap.get('id');
+
+    this.featuresService.getFeatureById(featureId).subscribe({
+      next: (res) => {
+        this.feature = res;
+        console.log('Feature details loaded:', res);
+      },
+      error: (error) => {
+        console.error('Error loading feature details:', error);
+      }
+    });
+  }
 }
